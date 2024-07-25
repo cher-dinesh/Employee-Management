a472e586f106e827714c4cd99de7b44280b22a96
cher-dinesh <139626471+cher-dinesh@users.noreply.github.com>
2024-07-25 16:45:05 +0530

    Update login.php

--- login.php
+++ login.php
@@ -12,22 +12,22 @@
             <div class="col-md-6">
                 <div class="card">
                     <div class="card-header">Login</div>
-                    <div class="card-body">
-                        <form action="authenticate.php" method="POST">
-                            <div class="form-group">
-                                <label for="username">Username</label>
-                                <input type="text" class="form-control" id="username" name="username" required>
-                            </div>
-                            <div class="form-group">
-                                <label for="password">Password</label>
-                                <input type="password" class="form-control" id="password" name="password" required>
-                            </div>
-                            <button type="submit" class="btn btn-primary">Login</button>
-                        </form>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
+                        <div class="card-body">
+                            <form action="authenticate.php" method="POST">
+                                <div class="form-group">
+                                    <label for="username">Username</label>
+                                    <input type="text" class="form-control" id="username" name="username" required>
+                                </div>
+                                <div class="form-group">
+                                    <label for="password">Password</label>
+                                    <input type="password" class="form-control" id="password" name="password" required>
+                                </div>
+                                <button type="submit" class="btn btn-primary">Login</button>
+                            </form>
+                        </div>
+                  </div>
+              </div>
+          </div>
+      </div>
 </body>
 </html>
